aaf41af8c40660dfa2de785c9d3cfdfb4e9a8c84
wilmouths <wilmouthsteven@gmail.com>
2021-04-30 17:42:31 +0200

    remove unsed import

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 exports.spaces = require('./lib/spaces');
-exports.objects = require('./lib/objects');
 exports.dashboard = require('./lib/dashboard');
 exports.users = require('./lib/users');
 exports.roles = require('./lib/roles');
