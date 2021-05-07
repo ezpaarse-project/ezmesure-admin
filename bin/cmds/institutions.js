0c33d5b84195f5bfb40e89f674f6567d7b1ca9e9
wilmouths <wilmouthsteven@gmail.com>
2021-05-07 15:20:36 +0200

    i18n: adding i18n on institutions commands

--- bin/cmds/institutions.js
+++ bin/cmds/institutions.js
@@ -1,5 +1,7 @@
+const { i18n } = global;
+
 exports.command = 'institutions <command>';
-exports.desc = 'Manage institutions <command>: list, get, export, import';
+exports.desc = i18n.t('institutions.description');
 exports.handler = function handler() {};
 exports.builder = function builder(yargs) {
   return yargs.commandDir('institutions');
