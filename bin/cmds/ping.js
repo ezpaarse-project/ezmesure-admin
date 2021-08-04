3d1522e6d0340655d55c8ab88962ac0fcfd52fad
wilmouths <wilmouthsteven@gmail.com>
2021-08-04 13:12:16 +0200

    feat: adding ping command

--- bin/cmds/ping.js
+++ bin/cmds/ping.js
@@ -6,7 +6,7 @@ const ezmesure = require('../../lib/app/ezmesure');
 exports.command = 'ping';
 exports.desc = i18n.t('ping.description');
 exports.builder = function builder() {};
-exports.handler = async function handler(argv) {
+exports.handler = async function handler() {
   let elasticPing;
   try {
     elasticPing = await elasticLib.ping();
