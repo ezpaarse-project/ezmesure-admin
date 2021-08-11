f9bd848221ad45be22fde994a61fc1ef493b2b26
wilmouths <wilmouthsteven@gmail.com>
2021-08-11 17:06:11 +0200

    remove reporting on space cmd

--- bin/cmds/spaces/reporting.js
+++
@@ -1,8 +0,0 @@
-const { i18n } = global;
-
-exports.command = 'reporting <command>';
-exports.desc = i18n.t('spaces.reporting.description');
-exports.handler = function handler() {};
-exports.builder = function builder(yargs) {
-  return yargs.commandDir('reporting');
-};
