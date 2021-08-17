3dee914585b945212a0d9e41c382d923608531fa
wilmouths <wilmouthsteven@gmail.com>
2021-08-17 10:58:01 +0200

    test: check if AUTH env vars are set

--- tests/utils/login.js
+++ tests/utils/login.js
@@ -5,10 +5,15 @@ const commandFile = path.resolve(process.cwd(), 'ezmesure-admin');
 
 const { EZMESURE_ADMIN_USERNAME, EZMESURE_ADMIN_PASSWORD } = process.env;
 
-module.exports = () => exec(commandFile, [
-  'login',
-  '--username',
-  EZMESURE_ADMIN_USERNAME,
-  '--password',
-  EZMESURE_ADMIN_PASSWORD,
-]);
+module.exports = () => {
+  expect(EZMESURE_ADMIN_USERNAME).toBeDefined();
+  expect(EZMESURE_ADMIN_PASSWORD).toBeDefined();
+
+  exec(commandFile, [
+    'login',
+    '--username',
+    EZMESURE_ADMIN_USERNAME,
+    '--password',
+    EZMESURE_ADMIN_PASSWORD,
+  ]);
+};
