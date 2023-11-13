bba520f7e83be058443580d38a0f7cc74614fe13
oxypomme <oxytom8@gmail.com>
2023-11-13 16:30:07 +0100

    feat(migrate): added logs for import

--- bin/cmds/migrate/import.js
+++ bin/cmds/migrate/import.js
@@ -3,6 +3,7 @@ const { i18n } = global;
 const https = require('https');
 const path = require('path');
 const fs = require('fs');
+const fsp = require('fs/promises');
 const readline = require('readline');
 
 const cliProgress = require('cli-progress');
@@ -22,6 +23,11 @@ exports.builder = function builder(yargs) {
       describe: i18n.t('import.options.bulkSize'),
       type: 'number',
       default: 15,
+    })
+    .option('o', {
+      alias: 'out',
+      describe: i18n.t('migrate.apply.options.out'),
+      type: 'string',
     })
     .option('k', {
       alias: 'insecure',
@@ -46,163 +52,163 @@ async function readJSONL(filePath) {
   });
 }
 
-// TODO show errors !
+async function importJSONL(opts) {
+  const rl = await readJSONL(opts.filePath);
+  const logFile = fs.createWriteStream(opts.logPath);
+  const data = [];
 
-async function importUsers(filePath, bulkSize) {
-  const rl = await readJSONL(filePath);
-  let data = [];
-  let i = 0;
+  const multiBar = new cliProgress.MultiBar(
+    {
+      format: chalk.grey('    {bar} {percentage}% | {value}/{total}'),
+    },
+    cliProgress.Presets.shades_classic,
+  );
+  const bar = multiBar.create(0, 0);
 
+  for await (const line of rl) {
+    data.push(JSON.parse(line));
+    bar.setTotal(bar.total + 1);
+  }
+
+  const counters = {
+    errors: 0,
+    conflicts: 0,
+    created: 0,
+    total: 0,
+  };
+  const chunkSize = opts.bulkSize || data.length;
+  console.log(chalk.grey(`  Using chunks of: ${chunkSize}`));
+  for (let i = 0; i < data.length; i += chunkSize) {
+    const chunk = data.slice(i, i + chunkSize);
+    const { data: response } = await opts.importer(chunk);
+
+    // update counters
+    counters.errors += response.errors ?? 0;
+    counters.conflicts += response.conflicts ?? 0;
+    counters.created += response.created ?? 0;
+
+    const total = (response.errors ?? 0) + (response.conflicts ?? 0) + (response.created ?? 0);
+    bar.increment(total);
+    counters.total += total;
+
+    // logging every item
+    for (const item of (response.items ?? [])) {
+      let message;
+      switch (item.status) {
+        case 'error':
+          message = `error: ${item.message || 'Unknown error from API'}`;
+          break;
+        case 'conflict':
+          message = `warn: ${item.message || 'Unknown warning from API'}`;
+          break;
+
+        default:
+          break;
+      }
+
+      if (message) {
+        logFile.write(`${message}\n${JSON.stringify(item.data ?? {})}\n\n`);
+      }
+    }
+  }
+
+  multiBar.stop();
+  logFile.end();
+  return counters;
+}
+
+async function importUsers(opts) {
   console.log(chalk.blue('i Importing users...'));
   console.group();
 
-  const bar = new cliProgress.SingleBar(
-    {
-      format: chalk.grey('    {bar} {percentage}% | {value}/{total}'),
-    },
-    cliProgress.Presets.shades_classic,
-  );
-  bar.start(0, 0);
+  const counters = await importJSONL({
+    filePath: opts.filePath,
+    bulkSize: opts.bulkSize,
+    logPath: opts.logPath,
+    importer: (chunk) => users.import(chunk),
+  });
 
-  for await (const line of rl) {
-    i += 1;
-    data.push(JSON.parse(line));
-    bar.setTotal(bar.total + 1);
-    if (i === bulkSize) {
-      // import users
-      await users.import(data);
-      bar.increment(data.length);
-      i = 0;
-      data = [];
-    }
-  }
-
-  if (data.length > 0) {
-    await users.import(data);
-    bar.increment(data.length);
-  }
-
-  bar.stop();
-  console.log(chalk.green(`✔️ ${bar.total} users imported`));
+  console.log(chalk.green(`✔️ ${counters.total} users imported (${counters.created} created, ${chalk.yellow(counters.conflicts)} conflicts, ${chalk.red(counters.errors)} errors)`));
   console.groupEnd();
 }
 
-async function importInstitutions(filePath, bulkSize) {
-  const rl = await readJSONL(filePath);
-  let data = [];
-  let i = 0;
-
+async function importInstitutions(opts) {
   console.log(chalk.blue('i Importing institutions...'));
   console.group();
 
-  const bar = new cliProgress.SingleBar(
-    {
-      format: chalk.grey('    {bar} {percentage}% | {value}/{total}'),
-    },
-    cliProgress.Presets.shades_classic,
-  );
-  bar.start(0, 0);
+  const counters = await importJSONL({
+    filePath: opts.filePath,
+    bulkSize: opts.bulkSize,
+    logPath: opts.logPath,
+    importer: (chunk) => institutions.import(chunk),
+  });
 
-  for await (const line of rl) {
-    i += 1;
-    data.push(JSON.parse(line));
-    if (i === bulkSize) {
-      // import institutions, repo, space, members
-      await institutions.import(data);
-      bar.increment(data.length);
-      i = 0;
-      data = [];
-    }
-  }
-
-  if (data.length > 0) {
-    await institutions.import(data);
-    bar.increment(data.length);
-  }
-
-  bar.stop();
-  console.log(chalk.green(`✔️ ${bar.total} institutions, repositories, spaces and memberships imported`));
+  console.log(chalk.green(`✔️ ${counters.total} institutions imported (${counters.created} created, ${chalk.yellow(counters.conflicts)} conflicts, ${chalk.red(counters.errors)} errors)`));
   console.groupEnd();
 }
 
-async function importSushiEndpoints(filePath, bulkSize) {
-  const rl = await readJSONL(filePath);
-  let data = [];
-  let i = 0;
-
+async function importSushiEndpoints(opts) {
   console.log(chalk.blue('i Importing sushi endpoints...'));
   console.group();
 
-  const bar = new cliProgress.SingleBar(
-    {
-      format: chalk.grey('    {bar} {percentage}% | {value}/{total}'),
-    },
-    cliProgress.Presets.shades_classic,
-  );
-  bar.start(0, 0);
+  const counters = await importJSONL({
+    filePath: opts.filePath,
+    bulkSize: opts.bulkSize,
+    logPath: opts.logPath,
+    importer: (chunk) => sushiEndpoint.import(chunk),
+  });
 
-  for await (const line of rl) {
-    i += 1;
-    data.push(JSON.parse(line));
-    bar.setTotal(bar.total + 1);
-    if (i === bulkSize) {
-      // import sushiEndpoint
-      await sushiEndpoint.import(data);
-      bar.increment(data.length);
-      i = 0;
-      data = [];
-    }
-  }
-
-  if (data.length > 0) {
-    await sushiEndpoint.import(data);
-    bar.increment(data.length);
-  }
-
-  bar.stop();
-  console.log(chalk.green(`✔️ ${bar.total} sushi endpoints imported`));
+  console.log(chalk.green(`✔️ ${counters.total} sushis endpoints imported (${counters.created} created, ${chalk.yellow(counters.conflicts)} conflicts, ${chalk.red(counters.errors)} errors)`));
   console.groupEnd();
 }
 
 exports.handler = async function handler(argv) {
-  const { exportedpath, bulkSize, insecure } = argv;
+  const {
+    exportedpath,
+    bulkSize,
+    insecure,
+    out,
+  } = argv;
 
   if (!fs.existsSync(exportedpath)) {
     console.error('No directory specified');
     return;
   }
 
+  // prepare out folder
+  let outFolder = path.resolve(out ?? '');
+  if (!out) {
+    outFolder = path.resolve(
+      path.dirname(exportedpath),
+      `${path.basename(exportedpath)}_imported`,
+    );
+  }
+  await fsp.mkdir(outFolder, { recursive: true });
+
   if (insecure) {
     ezmesure.defaults.httpsAgent = new https.Agent({ rejectUnauthorized: false });
   }
 
-  const filePathUsers = path.resolve(exportedpath, 'users.jsonl');
-  const filePathSushiEndpoints = path.resolve(exportedpath, 'sushis.jsonl');
-  const filePathInstitutions = path.resolve(exportedpath, 'institutions.jsonl');
-
   try {
-    await importUsers(filePathUsers, bulkSize);
+    await importUsers({
+      filePath: path.resolve(exportedpath, 'users.jsonl'),
+      bulkSize,
+      logPath: path.join(outFolder, 'user.log'),
+    });
+    await importSushiEndpoints({
+      filePath: path.resolve(exportedpath, 'sushis.jsonl'),
+      bulkSize,
+      logPath: path.join(outFolder, 'sushis.log'),
+    });
+    await importInstitutions({
+      filePath: path.resolve(exportedpath, 'institutions.jsonl'),
+      bulkSize,
+      logPath: path.join(outFolder, 'institutions.log'),
+    });
+
+    console.log(chalk.green('✔️ Import successful'));
   } catch (error) {
-    console.error('error users');
-    console.error(error.response.data);
+    await fsp.writeFile(path.join(outFolder, 'error.log'), `${error}`, 'utf-8');
     throw error;
   }
-
-  try {
-    await importSushiEndpoints(filePathSushiEndpoints, bulkSize);
-  } catch (error) {
-    console.error('error sushiEndpoint');
-    console.error(error.response.data);
-    throw error;
-  }
-
-  try {
-    await importInstitutions(filePathInstitutions, bulkSize);
-  } catch (error) {
-    console.error('error institution');
-    console.error(error.response.data);
-    throw error;
-  }
-
-  console.log(chalk.green('✔️ Import successful'));
 };
