44fc09824922067eb915df67679dbb9e2a121f80
oxypomme <oxytom8@gmail.com>
2024-04-08 13:53:36 +0200

    feat(export): added command to export ezmesure management data

---
+++ bin/cmds/export.js
@@ -0,0 +1,116 @@
+const { i18n } = global;
+
+const path = require('node:path');
+const fs = require('node:fs');
+const fsp = require('node:fs/promises');
+
+const cliProgress = require('cli-progress');
+const chalk = require('chalk');
+const { format } = require('date-fns');
+
+const usersLib = require('../../lib/users');
+const sushiEndpointsLib = require('../../lib/sushiEndpoints');
+const institutionsLib = require('../../lib/institutions');
+
+exports.command = 'export';
+exports.desc = i18n.t('export.description');
+exports.builder = (yargs) => yargs
+  .option('o', {
+    alias: 'out',
+    describe: i18n.t('export.options.out'),
+    type: 'string',
+    default: format(new Date(), 'yyyy-MM-dd'),
+  });
+
+const exportData = async (opts) => {
+  console.log(chalk.blue(i18n.t('export.going', { type: opts.type })));
+  console.group();
+  const { data } = await opts.fetch();
+
+  console.log(chalk.grey(i18n.t('export.file', { type: opts.type })));
+  const fileStream = fs.createWriteStream(opts.outFile);
+
+  const bar = new cliProgress.SingleBar(
+    {
+      format: chalk.grey('    {bar} {percentage}% | {value}/{total}'),
+    },
+    cliProgress.Presets.shades_classic,
+  );
+  bar.start(data.length, 0);
+
+  let count = 0;
+  data.forEach((item) => {
+    if (!opts.filter || opts.filter(item)) {
+      fileStream.write(`${JSON.stringify(item)}\n`);
+      count += 1;
+    }
+
+    bar.increment();
+  });
+
+  fileStream.end();
+  bar.stop();
+  console.log(chalk.green(
+    i18n.t(
+      'export.ok',
+      {
+        count,
+        type: opts.type,
+        out: chalk.underline(fileStream.path),
+      },
+    ),
+  ));
+  console.groupEnd();
+};
+
+exports.handler = async function handler(argv) {
+  const { out } = argv;
+
+  const dataFolder = path.resolve(out);
+  await fsp.mkdir(dataFolder, { recursive: true });
+
+  try {
+    await exportData({
+      type: 'users',
+      outFile: path.join(dataFolder, 'users.jsonl'),
+      fetch: () => usersLib.getAll({ size: -1, source: '*' }),
+      filter: (item) => item.username !== 'ezmesure-admin',
+    });
+
+    await exportData({
+      type: 'sushis',
+      outFile: path.join(dataFolder, 'sushis.jsonl'),
+      fetch: () => sushiEndpointsLib.getAll({ size: -1 }),
+    });
+
+    await exportData({
+      type: 'institutions',
+      outFile: path.join(dataFolder, 'institutions.jsonl'),
+      fetch: async () => {
+        const { data, ...resp } = await institutionsLib.getAll({ size: -1, include: ['sushiCredentials', 'spaces', 'repositories', 'memberships'] });
+
+        console.log(chalk.gray(i18n.t('export.institutionSort')));
+        // ensuring that parent institutions are always before their children
+        // working with a copy of the data to avoid mistakes while iterating over it
+        [...data].forEach((institution, index) => {
+          if (!institution.parentInstitutionId) {
+            return;
+          }
+
+          const parentIndex = data.findIndex((i) => i.id === institution.parentInstitutionId);
+          data.splice(index, 1);
+          data.splice(parentIndex + 1, 0, institution);
+        });
+
+        return { data, ...resp };
+      },
+    });
+
+    console.log(chalk.green(i18n.t('export.dataOk', { out: chalk.underline(dataFolder) })));
+  } catch (error) {
+    const now = new Date();
+    console.log(chalk.grey(i18n.t('export.file', { type: 'error logs' })));
+    await fsp.writeFile(path.join(dataFolder, 'error.log'), `${now.toISOString()} error: ${error}`, 'utf-8');
+    throw error;
+  }
+};
